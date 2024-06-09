c093fc5930b355a18b4c8b6fdcd48e2e464ef634
Adriano Matos <adrianotaua@hotmail.com>
2024-06-08 21:07:01 -0300

    Tratamento de erros com `React Toastify`

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Frontend Salvatore Academy - Conectando FrontEnd com APIs Backend
 
+## ROTA
+
+- /personagens
+
+## COMANDOS
+
+- npm i
+- npm run dev
+- npm i react-toastify
+<!-- Biblioteca de notificação -->
+
+## NOTAS
+
 - alt + shift + o: organiza imports
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import Card from "./components/Card/Card";
 
@@ -8,22 +10,33 @@ function App() {
   async function fetchData() {
     const apiUrl =
       "https://backend-integracao-com-frontend.onrender.com/personagens/";
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    setDevmons(data);
+
+    const response = await fetch(apiUrl).catch(function (error) {
+      toast.error("Erro ao carregar lista de Devmons.");
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      setDevmons(data);
+    } else {
+      toast.error("Erro ao carregar lista de Devmons.");
+    }
   }
-  
-	// Chama API só 1X
-	useEffect(function(){
-		fetchData();
-	}, [])
+
+  // Chama API só 1X
+  useEffect(function () {
+    fetchData();
+  }, []);
 
   return (
-    <div className="cards">
-      {devmons.map(function (devmon) {
-        return <Card key={devmon.nome} item={devmon} />;
-      })}
-    </div>
+    <>
+      <div className="cards">
+        {devmons.map(function (devmon) {
+          return <Card key={devmon.nome} item={devmon} />;
+        })}
+      </div>
+      <ToastContainer />
+    </>
   );
 }
 export default App;
